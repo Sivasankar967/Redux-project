83105c78da92f1ac2749b274a68e48fd762b82d5
Sivasankar967 <sankarsiva1859@gmail.com>
2022-08-24 04:43:49 -0700

    Redux

--- src/Container/Header.js
+++ src/Container/Header.js
@@ -4,7 +4,7 @@ const Header =()=>{
     return(
         <div className='ui fixed menu'>
            <div className='ui container center'>
-            <h2> Siva Shop</h2>
+            <h2>  Buy Something</h2>
             </div>
 
         </div>
